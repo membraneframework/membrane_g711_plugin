efb3e2e665805188c653d8d1523d68ff4c9d7917
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-18 12:20:25 +0200

    Bring back previous encode/docode examples

--- examples/decode_example.exs
+++ examples/decode_example.exs
@@ -7,26 +7,19 @@ Logger.configure(level: :info)
 Mix.install([
   {:membrane_g711_plugin, path: __DIR__ |> Path.join("..") |> Path.expand(), override: true},
   :membrane_file_plugin,
-  :req
+  :membrane_hackney_plugin
 ])
-
-sample_url =
-  "https://raw.githubusercontent.com/membraneframework/static/gh-pages/samples/beep-alaw-8kHz-mono.raw"
-
-sample_path = Path.join(System.tmp_dir!(), "beep-alaw-8kHz-mono.raw")
-
-unless File.exists?(sample_path) do
-  response = Req.get!(sample_url)
-  File.write!(sample_path, response.body)
-end
 
 defmodule Decoding.Pipeline do
   use Membrane.Pipeline
 
   @impl true
-  def handle_init(_ctx, sample_path) do
+  def handle_init(_ctx, _opts) do
     spec =
-      child(:source, %Membrane.File.Source{location: sample_path})
+      child(:source, %Membrane.Hackney.Source{
+        location:
+          "https://raw.githubusercontent.com/membraneframework/static/gh-pages/samples/beep-alaw-8kHz-mono.raw"
+      })
       |> child(:decoder, Membrane.G711.Decoder)
       |> child(:sink, %Membrane.File.Sink{location: "output.raw"})
 
@@ -45,19 +38,11 @@ defmodule Decoding.Pipeline do
 end
 
 # Start and monitor the pipeline
-{:ok, _supervisor_pid, pipeline_pid} =
-  Membrane.Pipeline.start_link(Decoding.Pipeline, sample_path)
-
+{:ok, _supervisor_pid, pipeline_pid} = Membrane.Pipeline.start_link(Decoding.Pipeline)
 ref = Process.monitor(pipeline_pid)
 
 # Wait for the pipeline to finish
 receive do
-  {:DOWN, ^ref, :process, _pipeline_pid, reason} ->
-    if reason in [:shutdown, :normal] do
-      IO.puts("Pipeline finished: #{inspect(reason)}")
-      System.halt(0)
-    else
-      IO.puts(:stderr, "Pipeline failed: #{inspect(reason)}")
-      System.halt(1)
-    end
+  {:DOWN, ^ref, :process, _pipeline_pid, _reason} ->
+    System.stop()
 end
