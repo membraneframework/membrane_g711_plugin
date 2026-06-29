737974aaabd73697cac71b4b32ef916d46c83676
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 12:12:25 +0200

    Rename prepend_llms_links to append_llms_links

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Membrane.G711.Plugin.Mixfile do
       name: "Membrane G711 Plugin",
       source_url: @github_url,
       docs: docs(),
-      aliases: [docs: ["docs", &prepend_llms_links/1]]
+      aliases: [docs: ["docs", &append_llms_links/1]]
     ]
   end
 
@@ -91,7 +91,7 @@ defmodule Membrane.G711.Plugin.Mixfile do
     ]
   end
 
-  defp prepend_llms_links(_) do
+  defp append_llms_links(_args) do
     output_dir = docs()[:output] || "doc"
     path = Path.join(output_dir, "llms.txt")
 
